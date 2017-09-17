10ec8ab8c8a00e87f81a94155c7d867b1f9becad
oleg.kochura <o.kochura@viseven.com>
2017-09-18 02:55:29 +0300

    add build

--- src/api/api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const url = 'https://api.adorable.io/avatars/list';
-const proxy = 'https://cors-anywhere.herokuapp.com';
-
-export function api() {
-	return axios.get(`${proxy}/${url}`);
-}
--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import './css/main.css';
-import './css/layout.css';
-import './css/fonts.css';
-
-ReactDOM.render(<App />, document.getElementById('root'));
